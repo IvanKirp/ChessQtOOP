d922b70e932964d420b8f57dde62d566ee83b2ae
Ivan <ivankirpicev879@gmail.com>
2026-05-28 02:01:40 +0300

    some changes

--- build/Desktop-Debug/CMakeFiles/ChessQtOOP.dir/ChessQtOOP_autogen/EWIEGA46WW/qrc_resources.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/ChessQtOOP.dir/ChessQtOOP_autogen/EWIEGA46WW/qrc_resources.cpp.o: \
-  /home/ivan/Документы/453503/ООП/ChessQtOOP/build/Desktop-Debug/ChessQtOOP_autogen/EWIEGA46WW/qrc_resources.cpp
